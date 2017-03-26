a3004909e253d3b054940b5b35b9a6810c1ea2f0
Andi Kluge <andi.kluge@mobi4friends.de>
2017-03-26 14:31:24 +0200

    Step 3

--- src/main/java/net/team33/hash/Cache.java
+++ src/main/java/net/team33/hash/Cache.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public class Cache {
+class Cache {
 
     private final Map<Path, Entry> entries = new HashMap<>(0);
 
-    public Entry entry(final Path path) {
+    Entry entry(final Path path) {
         return Optional.ofNullable(entries.get(path)).orElseGet(() -> {
             final Entry entry = new Entry();
             entries.put(path, entry);
@@ -18,11 +18,11 @@ public class Cache {
         });
     }
 
-    public class Entry {
+    class Entry {
 
         private final Map<String, Details> map = new HashMap<>(0);
 
-        public Details details(final String name) {
+        Details details(final String name) {
             return Optional.ofNullable(map.get(name)).orElseGet(() -> {
                 final Details result = new Details();
                 map.put(name, result);
@@ -31,24 +31,24 @@ public class Cache {
         }
     }
 
-    public class Details {
+    class Details {
 
         private long update;
         private BigInteger hash;
 
-        public long getUpdate() {
+        long getUpdate() {
             return update;
         }
 
-        public void setUpdate(final long timeStamp) {
+        void setUpdate(final long timeStamp) {
             update = timeStamp;
         }
 
-        public BigInteger getHash() {
+        BigInteger getHash() {
             return hash;
         }
 
-        public void setHash(final BigInteger value) {
+        void setHash(final BigInteger value) {
             hash = value;
         }
     }
--- src/main/java/net/team33/hash/Hash.java
+++ src/main/java/net/team33/hash/Hash.java
@@ -6,11 +6,11 @@ import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class Hash {
+class Hash {
 
     private static final int BUFFER_SIZE = 17;
 
-    public static BigInteger create(final Path path) throws IOException {
+    static BigInteger create(final Path path) throws IOException {
         final byte[] values = new byte[BUFFER_SIZE];
         final byte[] buffer = new byte[BUFFER_SIZE];
         try (final InputStream in = Files.newInputStream(path)) {
