9bea3f9980ad00c78e59ed1642bbef5d3b9dca05
Andi Kluge <andi.kluge@mobi4friends.de>
2017-03-27 13:51:21 +0200

    Sieve is implemented

--- src/main/java/net/team33/hash/Main.java
+++ src/main/java/net/team33/hash/Main.java
@@ -20,17 +20,37 @@ import java.util.stream.Stream;
 @SuppressWarnings("UseOfSystemOutOrSystemErr")
 public final class Main {
 
+    private static final Consumer<Map.Entry<BigInteger, List<Path>>> DEFAULT_SIEVE_JOB =
+            e -> System.out.printf("%s (%d): %s%n", e.getKey(), e.getValue().size(), e.getValue());
+
     private final Registry registry = new Registry();
     private final Map<BigInteger, List<Path>> pathsMap = new HashMap<>();
     private final List<Throwable> problems = new LinkedList<>();
     private final List<Path> ignored = new LinkedList<>();
     private final List<Path> failed = new LinkedList<>();
     private final List<Path> skipped = new LinkedList<>();
+    private final Consumer<Map.Entry<BigInteger, List<Path>>> sieveJob;
+    private final Path trash;
+
+    private Main(final Path trash) {
+        this.trash = trash;
+        this.sieveJob = null == trash ? DEFAULT_SIEVE_JOB : this::sieve;
+    }
 
     public static void main(final String[] args) throws IOException, InterruptedException {
         final long time0 = System.currentTimeMillis();
-        final Main main = new Main();
-        main.process(Stream.of(args)
+        final Path trash;
+        final int head;
+        if (1 < args.length && "SIEVE".equals(args[0].toUpperCase())) {
+            trash = Paths.get(args[1]).toAbsolutePath().normalize();
+            head = 2;
+        } else {
+
+            trash = null;
+            head = 0;
+        }
+        final Main main = new Main(trash);
+        main.process(Stream.of(args).skip(head)
                 .map(arg -> Paths.get(arg).toAbsolutePath().normalize())
                 .collect(Collectors.toList())
                 .iterator());
@@ -38,14 +58,14 @@ public final class Main {
         System.out.printf("%nDuplicate files (total %d):%n", main.pathsMap.size());
         main.pathsMap.entrySet().stream()
                 .filter(e -> 1 < e.getValue().size())
-                .forEach(e -> System.out.printf("%s (%d): %s%n", e.getKey(), e.getValue().size(), e.getValue()));
+                .forEach(main.sieveJob);
 
         //print(main.ignored);
         print(main.failed);
         print(main.skipped);
         print(main.problems, "%nProblems (%d):%n", problem -> problem.printStackTrace(System.out));
 
-        System.out.printf("%n%s seconds%n", (System.currentTimeMillis() - time0) / 1000);
+        System.out.printf("%n%s seconds%n", ((System.currentTimeMillis() - time0) / 1000.0));
     }
 
     private static void print(final List<?> collected) {
@@ -59,6 +79,22 @@ public final class Main {
         }
     }
 
+    private void sieve(final Map.Entry<BigInteger, List<Path>> entry) {
+        entry.getValue().stream().skip(1).forEach((path) -> sieve(entry.getKey(), path));
+    }
+
+    private void sieve(final BigInteger key, final Path path) {
+        final Path parent = trash.getParent();
+        final Path target = trash.resolve(parent.relativize(path));
+        try {
+            Files.createDirectories(target.getParent());
+            Files.move(path, target);
+            System.out.printf("%s: moved <%s> to <%s>%n", key, path, target);
+        } catch (IOException e) {
+            problems.add(e);
+        }
+    }
+
     private void process(final Iterator<Path> paths) {
         while (paths.hasNext()) {
             process(paths.next());
@@ -67,6 +103,8 @@ public final class Main {
 
     private void process(final Path path) {
         if (Registry.FILE_NAME.equals(path.getFileName().toString())) {
+            ignored.add(path);
+        } else if (path.equals(trash)) {
             ignored.add(path);
         } else if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
             onDirectory(path);
@@ -95,7 +133,7 @@ public final class Main {
         final Registry.Details details = entry.details(path.getFileName().toString());
         try {
             final long timeStamp = Files.getLastModifiedTime(path, LinkOption.NOFOLLOW_LINKS).toMillis();
-            if (details.getUpdate() < timeStamp) {
+            if (details.getUpdate() != timeStamp) {
                 details.setUpdate(timeStamp);
                 details.setHash(Hash.create(path));
             }
